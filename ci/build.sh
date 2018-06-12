14108884b507a291e8cf9f7e6cb6ed964155213c
Ben Hale <bhale@pivotal.io>
2018-06-12 13:44:49 -0700

    Build Improvements
    This change updates the build so that the embedded PostgreSQL can run.  This
    means that alpine is out and the tests must be run as non-root.  A Dockerfile
    has been created that encapsulates these requirements.

--- ci/build.sh
+++ ci/build.sh
@@ -1,9 +1,12 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 
 [[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s $PWD/maven $HOME/.m2
+[[ -d $PWD/postgresql && ! -d $HOME/.embedpostgresql ]] && ln -s $PWD/postgresql $HOME/.embedpostgresql
 
 repository=$(pwd)/distribution-repository
 
+chown -R test:test /tmp/build
+
 cd r2dbc-postgresql
-./mvnw deploy -DaltDeploymentRepository=distribution::default::file://${repository}
+exec sudo -u test -E ./mvnw deploy -DaltDeploymentRepository=distribution::default::file://${repository}
