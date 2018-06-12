07eb1f93db425fdb2bce5e7714fa3db59ee4adf3
Ben Hale <bhale@pivotal.io>
2018-06-12 14:05:41 -0700

    Polishing

--- ci/build.sh
+++ ci/build.sh
@@ -4,7 +4,7 @@
 [[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s $PWD/maven $HOME/.m2
 [[ -d $PWD/postgresql && ! -d $HOME/.embedpostgresql ]] && ln -s $PWD/postgresql $HOME/.embedpostgresql
 
-repository=$(pwd)/distribution-repository
+repository=$(pwd)/r2dbc-postgresql-artifactory
 
 chown -R test:test /tmp/build
 
