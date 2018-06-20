950ed488f5b3a08d43622a14d937f1345f0711ab
Ben Hale <bhale@pivotal.io>
2018-06-20 14:59:00 +0100

    Dependencies from Concourse Resource
    This change adds support to the CI script and the POM to get upstream
    dependencies from a Concourse resource.

--- ci/build.sh
+++ ci/build.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+set -e -u
 
 [[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s $PWD/maven $HOME/.m2
 [[ -d $PWD/postgresql && ! -d $HOME/.embedpostgresql ]] && ln -s $PWD/postgresql $HOME/.embedpostgresql
 
-repository=$(pwd)/r2dbc-postgresql-artifactory
+r2dbc_postgresql_artifactory=$(pwd)/r2dbc-postgresql-artifactory
+r2dbc_spi_artifactory=$(pwd)/r2dbc-spi-artifactory
 
-chown -R test:test /tmp/build
+rm -rf $HOME/.m2/repository/io/r2dbc 2> /dev/null || :
 
 cd r2dbc-postgresql
-exec sudo -u test -E ./mvnw deploy -DaltDeploymentRepository=distribution::default::file://${repository}
+./mvnw deploy \
+    -DaltDeploymentRepository=distribution::default::file://${r2dbc_postgresql_artifactory} \
+    -Dr2dbcSpiArtifactory=file://${r2dbc_spi_artifactory}
