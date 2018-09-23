eec47682b103b8815ce372a41f63dae6019029af
Ben Hale <bhale@pivotal.io>
2018-09-22 17:39:40 -0700

    Release scripts

---
+++ ci/release.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e -u
+
+[[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s $PWD/maven $HOME/.m2
+[[ -d $PWD/postgresql && ! -d $HOME/.embedpostgresql ]] && ln -s $PWD/postgresql $HOME/.embedpostgresql
+
+r2dbc_postgresql_artifactory=$(pwd)/r2dbc-postgresql-artifactory
+
+rm -rf $HOME/.m2/repository/io/r2dbc 2> /dev/null || :
+
+cd r2dbc-postgresql
+./mvnw deploy \
+    -DaltDeploymentRepository=distribution::default::file://${r2dbc_postgresql_artifactory}
