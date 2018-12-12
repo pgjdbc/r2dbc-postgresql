0bf924132487a98eb7ee5735646a594f0cc36c3a
igor-suhorukov <igor.suhorukov@gmail.com>
2018-12-13 02:15:59 +0300

    Polishing
    Remove unused imports.
    
    [resolves #46]
    
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- src/test/java/io/r2dbc/postgresql/PostgresqlExample.java
+++ src/test/java/io/r2dbc/postgresql/PostgresqlExample.java
@@ -25,8 +25,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-import java.util.Arrays;
-
 final class PostgresqlExample implements Example<String> {
 
     @RegisterExtension
