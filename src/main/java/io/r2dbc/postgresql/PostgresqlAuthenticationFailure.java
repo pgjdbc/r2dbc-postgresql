153802c6d433b7fc06a39dc183e92b1c916e51c7
Hebert Coelho <uaihebert@gmail.com>
2019-02-07 19:27:22 +0100

    create an exception that should be used for authentication errors

---
+++ src/main/java/io/r2dbc/postgresql/PostgresqlAuthenticationFailure.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2017-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.r2dbc.postgresql;
+
+import io.r2dbc.postgresql.message.backend.Field;
+
+import java.util.List;
+
+/**
+ * An exception that represents a authentication error.
+ */
+public class PostgresqlAuthenticationFailure extends PostgresqlServerErrorException {
+
+    /**
+     * Creates a new exception.
+     *
+     * @param fields the fields to be used to populate the exception
+     * @throws IllegalArgumentException if {@code fields} is {@code null}
+     */
+    public PostgresqlAuthenticationFailure(final List<Field> fields) {
+        super(fields);
+    }
+
+    @Override
+    public String toString() {
+        return "PostgresqlAuthenticationFailure: " + getMessage();
+    }
+}
--- src/main/java/io/r2dbc/postgresql/PostgresqlServerErrorException.java
+++ src/main/java/io/r2dbc/postgresql/PostgresqlServerErrorException.java
@@ -52,7 +52,7 @@ import static io.r2dbc.postgresql.message.backend.Field.FieldType.WHERE;
 /**
  * An exception that represents a server error.  This exception is a direct translation of the {@link ErrorResponse} message.
  */
-public final class PostgresqlServerErrorException extends R2dbcException {
+public class PostgresqlServerErrorException extends R2dbcException {
 
     private final String code;
 
