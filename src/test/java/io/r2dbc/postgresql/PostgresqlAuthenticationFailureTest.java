04742fc26aecde6cf4b26c2356ad39a2bd7625f6
Hebert Coelho <uaihebert@gmail.com>
2019-02-07 19:30:26 +0100

    add a test that will check if the auth exception is being thrown

---
+++ src/test/java/io/r2dbc/postgresql/PostgresqlAuthenticationFailureTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2017-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.r2dbc.postgresql;
+
+import io.r2dbc.postgresql.util.PostgresqlServerExtension;
+import io.r2dbc.spi.ConnectionFactories;
+import io.r2dbc.spi.ConnectionFactory;
+import io.r2dbc.spi.ConnectionFactoryOptions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import reactor.core.publisher.Mono;
+
+import static io.r2dbc.postgresql.PostgresqlConnectionFactoryProvider.POSTGRESQL_DRIVER;
+import static io.r2dbc.spi.ConnectionFactoryOptions.DATABASE;
+import static io.r2dbc.spi.ConnectionFactoryOptions.DRIVER;
+import static io.r2dbc.spi.ConnectionFactoryOptions.HOST;
+import static io.r2dbc.spi.ConnectionFactoryOptions.PASSWORD;
+import static io.r2dbc.spi.ConnectionFactoryOptions.PORT;
+import static io.r2dbc.spi.ConnectionFactoryOptions.USER;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class PostgresqlAuthenticationFailureTest {
+
+    @RegisterExtension
+    static final PostgresqlServerExtension SERVER = new PostgresqlServerExtension();
+
+    @Test
+    public void authExceptionCausedByWrongCredentials() {
+        final ConnectionFactory connectionFactory = ConnectionFactories.get(ConnectionFactoryOptions.builder()
+            .option(DRIVER, POSTGRESQL_DRIVER)
+            .option(DATABASE, SERVER.getDatabase())
+            .option(HOST,  SERVER.getHost())
+            .option(PORT, SERVER.getPort())
+            .option(PASSWORD, "SUPER WRONG PASSWORD")
+            .option(USER, SERVER.getUsername())
+            .build());
+
+        assertThatExceptionOfType(PostgresqlAuthenticationFailure.class)
+            .isThrownBy(() -> Mono.from(connectionFactory.create()).block())
+            .withMessage("password authentication failed for user \"test\"");
+    }
+}
