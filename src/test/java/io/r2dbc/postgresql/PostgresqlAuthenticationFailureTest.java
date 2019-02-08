b46574a89001b1fc9427a85f0222b43ae776250a
Hebert Coelho <uaihebert@gmail.com>
2019-02-08 10:01:44 +0100

    add unit tests for authentication expcetion

--- src/test/java/io/r2dbc/postgresql/PostgresqlAuthenticationFailureTest.java
+++ src/test/java/io/r2dbc/postgresql/PostgresqlAuthenticationFailureTest.java
@@ -16,6 +16,7 @@
 
 package io.r2dbc.postgresql;
 
+import io.r2dbc.postgresql.message.backend.Field;
 import io.r2dbc.postgresql.util.PostgresqlServerExtension;
 import io.r2dbc.spi.ConnectionFactories;
 import io.r2dbc.spi.ConnectionFactory;
@@ -24,6 +25,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import reactor.core.publisher.Mono;
 
+import java.util.Collections;
+
 import static io.r2dbc.postgresql.PostgresqlConnectionFactoryProvider.POSTGRESQL_DRIVER;
 import static io.r2dbc.spi.ConnectionFactoryOptions.DATABASE;
 import static io.r2dbc.spi.ConnectionFactoryOptions.DRIVER;
@@ -31,12 +34,13 @@ import static io.r2dbc.spi.ConnectionFactoryOptions.HOST;
 import static io.r2dbc.spi.ConnectionFactoryOptions.PASSWORD;
 import static io.r2dbc.spi.ConnectionFactoryOptions.PORT;
 import static io.r2dbc.spi.ConnectionFactoryOptions.USER;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 final class PostgresqlAuthenticationFailureTest {
 
     @RegisterExtension
-    private static final PostgresqlServerExtension SERVER = new PostgresqlServerExtension();
+    static final PostgresqlServerExtension SERVER = new PostgresqlServerExtension();
 
     @Test
     void authExceptionCausedByWrongCredentials() {
@@ -52,5 +56,27 @@ final class PostgresqlAuthenticationFailureTest {
         assertThatExceptionOfType(PostgresqlAuthenticationFailure.class)
             .isThrownBy(() -> Mono.from(connectionFactory.create()).block())
             .withMessage("password authentication failed for user \"test\"");
+    }
+    
+    @Test
+    void initializesReason() {
+        PostgresqlAuthenticationFailure exception = new PostgresqlAuthenticationFailure(Collections.singletonList(new Field(Field.FieldType.MESSAGE, "Failed to Connect")));
+
+        assertThat(exception.getMessage()).isEqualTo("Failed to Connect");
+    }
+
+    @Test
+    void initializesSqlState() {
+        PostgresqlAuthenticationFailure exception = new PostgresqlAuthenticationFailure(Collections.singletonList(new Field(Field.FieldType.CODE, "7373")));
+
+        assertThat(exception.getSqlState()).isEqualTo("7373");
+    }
+
+    @Test
+    void skipsInitializationWithEmptyFields() {
+        PostgresqlAuthenticationFailure exception = new PostgresqlAuthenticationFailure(Collections.emptyList());
+
+        assertThat(exception.getSqlState()).isNull();
+        assertThat(exception.getMessage()).isNull();
     }
 }
