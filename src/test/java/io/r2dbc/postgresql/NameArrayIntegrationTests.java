396b758e5a2550fc5ae4d03fc9082a741ede51f1
Mark Paluch <mpaluch@vmware.com>
2020-12-09 12:13:24 +0100

    Polishing
    Reformat code, add license header.
    
    [#361][#362]

--- src/test/java/io/r2dbc/postgresql/NameArrayIntegrationTests.java
+++ src/test/java/io/r2dbc/postgresql/NameArrayIntegrationTests.java
@@ -1,6 +1,21 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.r2dbc.postgresql;
 
-import io.r2dbc.postgresql.api.PostgresqlConnection;
 import io.r2dbc.postgresql.api.PostgresqlResult;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.core.JdbcOperations;
@@ -10,7 +25,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class NameArrayIntegrationTests extends AbstractIntegrationTests {
 
-
     @Test
     void shouldReadArrayOfName() {
 
@@ -18,24 +32,21 @@ public class NameArrayIntegrationTests extends AbstractIntegrationTests {
         jdbcOperations.execute("DROP TABLE IF EXISTS name_table;");
         jdbcOperations.execute("CREATE TABLE name_table (n name)");
 
-        PostgresqlConnection connection = this.connectionFactory.create().block();
+        this.connection.createStatement("INSERT INTO name_table (n) VALUES('hello'), ('world'), (null)")
+            .execute()
+            .flatMap(PostgresqlResult::getRowsUpdated)
+            .as(StepVerifier::create)
+            .expectNextCount(1)
+            .verifyComplete();
 
-        connection.createStatement("INSERT INTO name_table (n) VALUES('hello'), ('world'), (null)")
-                .execute()
-                .flatMap(PostgresqlResult::getRowsUpdated)
-                .as(StepVerifier::create)
-                .expectNextCount(1)
-                .verifyComplete();
-
-        connection.createStatement("select array_agg(n) as names from name_table")
-                .execute()
-                .flatMap(it -> it.map((row, rowMetadata) -> row.get("names")))
-                .as(StepVerifier::create)
-                .assertNext(arr -> {
-                    assertThat((Object[]) arr).containsExactly(new Object[]{"hello", "world", null});
-                })
-                .verifyComplete();
-
-        connection.close().block();
+        this.connection.createStatement("select array_agg(n) as names from name_table")
+            .execute()
+            .flatMap(it -> it.map((row, rowMetadata) -> row.get("names")))
+            .as(StepVerifier::create)
+            .assertNext(arr -> {
+                assertThat((Object[]) arr).containsExactly(new Object[]{"hello", "world", null});
+            })
+            .verifyComplete();
     }
+
 }
