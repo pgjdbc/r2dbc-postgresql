8018584e755a15442406033b48a56b4c878b71bb
Sergey Starosek <sergey.starosek@weigandt-consulting.com>
2022-04-05 10:03:47 +0300

    Add `sslMode` option lowercase alias
    For easier JDBC URL adaption
    
    [resolves #486]

--- src/main/java/io/r2dbc/postgresql/PostgresqlConnectionFactoryProvider.java
+++ src/main/java/io/r2dbc/postgresql/PostgresqlConnectionFactoryProvider.java
@@ -181,6 +181,11 @@ public final class PostgresqlConnectionFactoryProvider implements ConnectionFact
     public static final Option<SSLMode> SSL_MODE = Option.valueOf("sslMode");
 
     /**
+     * Ssl mode alias (JDBC style). Default: disabled
+     */
+    public static final Option<SSLMode> SSL_MODE_ALIAS = Option.valueOf("sslmode");
+
+    /**
      * SSL key password
      */
     public static final Option<String> SSL_PASSWORD = Option.valueOf("sslPassword");
@@ -301,7 +306,18 @@ public final class PostgresqlConnectionFactoryProvider implements ConnectionFact
 
             return (SSLMode) it;
 
-        }).to(builder::sslMode);
+        }).to(builder::sslMode).otherwise(() -> {
+
+            mapper.from(SSL_MODE_ALIAS).map(it -> {
+
+                if (it instanceof String) {
+                    return SSLMode.fromValue(it.toString());
+                }
+
+                return (SSLMode) it;
+
+            }).to(builder::sslMode);
+        });
 
         mapper.fromTyped(SSL_CERT).to(builder::sslCert);
         mapper.fromTyped(SSL_CONTEXT_BUILDER_CUSTOMIZER).to(builder::sslContextBuilderCustomizer);
