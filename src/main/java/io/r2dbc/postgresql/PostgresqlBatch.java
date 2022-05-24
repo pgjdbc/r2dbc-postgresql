c74bd6e3439d812a772c1c64f60054de0d8706b0
toverdijk <tjesseoverdijk@gmail.com>
2022-05-24 17:11:07 +0200

    Rename SQL parser components.
    [#512][#513]

--- src/main/java/io/r2dbc/postgresql/PostgresqlBatch.java
+++ src/main/java/io/r2dbc/postgresql/PostgresqlBatch.java
@@ -40,7 +40,7 @@ final class PostgresqlBatch implements io.r2dbc.postgresql.api.PostgresqlBatch {
     public PostgresqlBatch add(String sql) {
         Assert.requireNonNull(sql, "sql must not be null");
 
-        if (!(PostgresqlSqlLexer.tokenize(sql).getParameterCount() == 0)) {
+        if (!(PostgresqlSqlParser.tokenize(sql).getParameterCount() == 0)) {
             throw new IllegalArgumentException(String.format("Statement '%s' is not supported.  This is often due to the presence of parameters.", sql));
         }
 
